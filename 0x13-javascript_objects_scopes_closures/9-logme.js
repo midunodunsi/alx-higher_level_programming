4b742450904063c0d92e525e188cf11fb7279cba
midunodunsi <midun99.odunsi@gmail.com>
2024-04-24 06:28:15 -0700

    logme

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -4,6 +4,6 @@ exports.logMe = function (item) {
 	if (typeof this.count === 'undefined') {
 		this.count = 0;
 	}
-	console.log(this.count + ':' + item);
+	console.log(this.count + ': ' + item);
 	this.count++;
 };
